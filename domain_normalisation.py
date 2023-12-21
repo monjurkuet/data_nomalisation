ed8dd0305e0eda139f6d96442c16408009b6a4ec
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-12-21 17:31:19 +0600

    Update

--- domain_normalisation.py
+++ domain_normalisation.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlparse
+import pandas as pd
 
 def parse_domain(url):
     urlfinal=urlparse(url).netloc.replace("www.", "")
@@ -7,3 +8,9 @@ def parse_domain(url):
     print(urlfinal)
     return urlfinal.lower()
 
+df=pd.read_csv('domains.csv')
+
+for i in range(len(df)):
+    df.loc[i,'domain']=parse_domain(df.loc[i,'URL'])
+
+df.to_csv('domains_op.csv',index=None)
